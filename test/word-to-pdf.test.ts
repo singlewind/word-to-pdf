772ad4ea7e10f800e2d70bd02e225eb9701fb6c9
Yue Zhou <yuezhou@geniesolutions.com.au>
2021-12-02 09:07:40 +1000

    Initial commit

---
+++ test/word-to-pdf.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as WordToPdf from '../lib/word-to-pdf-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/word-to-pdf-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WordToPdf.WordToPdfStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
